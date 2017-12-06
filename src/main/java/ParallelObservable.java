1273f8f0f74504899d952fa8536eea1be3f31375
Georgii_Goriachev <georgii_goriachev@epam.com>
2017-12-06 12:01:39 +0200

    - improve rx multithreading

--- src/main/java/ParallelObservable.java
+++ src/main/java/ParallelObservable.java
@@ -1,28 +1,50 @@
 import io.reactivex.Observable;
+import io.reactivex.functions.Action;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 
+import java.util.Timer;
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+
 /**
- * Created by gorge on 05.12.2017.
+ * https://praveer09.github.io/technology/2016/02/29/rxjava-part-3-multithreading/
  */
 public class ParallelObservable {
     public static void main(String[] args) {
-        Observable.range(0, 10)
-                .flatMap(i -> Observable.just(longOperation(i)))
-                .subscribeOn(Schedulers.newThread())
-                .observeOn(Schedulers.computation())
-                //.observeOn(Schedulers.newThread())
-                .subscribe(i -> System.out.println(Thread.currentThread() + " :" + i));
-
-    }
-
-    private static Integer longOperation(Integer i) {
-        Integer res = i += 2;
+        ParallelObservable app = new ParallelObservable();
+        Observable.fromCallable(thatReturnsNumberOne())     // the Observable
+                .subscribeOn(Schedulers.newThread())        // method tells the RxJava to perform the computation, declared as part of the Observable
+                .map(numberToString())                      // the Operator
+                .observeOn(Schedulers.newThread())          // subscriber on different thread
+                .subscribe(printResult());                  // the Subscriber
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println(Thread.currentThread() + " i=" + i);
-        return res;
+
+    }
+
+    private static Callable<Integer> thatReturnsNumberOne() {
+        return () -> {
+            System.out.println("Observable thread: " + Thread.currentThread().getName());
+            return 1;
+        };
+    }
+
+    private static Function<Integer, String> numberToString() {
+        return number -> {
+            System.out.println("Operator thread: " + Thread.currentThread().getName());
+            return String.valueOf(number);
+        };
+    }
+
+    private static Consumer<String> printResult() {
+        return result -> {
+            System.out.println("Subscriber thread: " + Thread.currentThread().getName());
+            System.out.println("Result: " + result);
+        };
     }
 }
--- src/main/java/java9/SimpleFlowable.java
+++ src/main/java/java9/SimpleFlowable.java
@@ -1,34 +1,38 @@
 package java9;
 
 import io.reactivex.Flowable;
-import io.reactivex.subscribers.DisposableSubscriber;
-import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
-
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created by gorge on 26.11.2017.
  */
-public class SimpleParallelFlowable {
+public class SimpleFlowable {
     public static void main(String[] args) {
         Flowable.range(0, 100)
                 .subscribe(new Subscriber<Integer>() {
+                    public Subscription subscription;
 
                     @Override
                     public void onSubscribe(Subscription subscription) {
-                        //subscription.request(10);
+                        this.subscription = subscription;
+                        subscription.request(10);
                     }
 
                     @Override
                     public void onNext(Integer i) {
                         System.out.println(i);
+                        if (i.intValue() == 5) {
+                            subscription.request(10);
+                        }
+                        if (i.intValue() == 20) {
+                            subscription.cancel();
+                        }
                     }
 
                     @Override
                     public void onError(Throwable throwable) {
-                        System.out.println(throwable);
+
                     }
 
                     @Override
